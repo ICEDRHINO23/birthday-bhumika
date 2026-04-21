804ba2207c13be2c109a759d38eb4edef78891cb
ICEDRHINO23 <abinavft@gmail.com>
2026-04-22 01:56:45 +0530

    Create scrapbook.js

---
+++ scrapbook.js
@@ -0,0 +1,21 @@
+let current = 0;
+
+const pages = document.querySelectorAll(".page");
+
+function nextPage() {
+  if (current < pages.length) {
+    pages[current].classList.add("flipped");
+    current++;
+  }
+}
+
+function prevPage() {
+  if (current > 0) {
+    current--;
+    pages[current].classList.remove("flipped");
+  }
+}
+
+function goHome() {
+  window.location.href = "index.html";
+}
