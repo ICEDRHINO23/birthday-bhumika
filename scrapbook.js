deb75e89cba301040e721c540f8f0aeee19793f5
ICEDRHINO23 <abinavft@gmail.com>
2026-04-22 02:07:56 +0530

    Update scrapbook.js

--- scrapbook.js
+++ scrapbook.js
@@ -1,21 +1,20 @@
-let current = 0;
+let current=0;
+const pages=document.querySelectorAll(".page");
 
-const pages = document.querySelectorAll(".page");
-
-function nextPage() {
-  if (current < pages.length) {
-    pages[current].classList.add("flipped");
-    current++;
-  }
+function nextPage(){
+if(current<pages.length){
+pages[current].classList.add("flipped");
+current++;
+}
 }
 
-function prevPage() {
-  if (current > 0) {
-    current--;
-    pages[current].classList.remove("flipped");
-  }
+function prevPage(){
+if(current>0){
+current--;
+pages[current].classList.remove("flipped");
+}
 }
 
-function goHome() {
-  window.location.href = "index.html";
+function goHome(){
+window.location.href="index.html";
 }
