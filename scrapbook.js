540a933ce60859fb0491fe273b258750376483d9
ICEDRHINO23 <abinavft@gmail.com>
2026-04-22 02:16:07 +0530

    Update scrapbook.js

--- scrapbook.js
+++ scrapbook.js
@@ -1,20 +1,28 @@
-let current=0;
-const pages=document.querySelectorAll(".page");
+let current = 0;
+const pages = document.querySelectorAll(".page");
 
-function nextPage(){
-if(current<pages.length){
-pages[current].classList.add("flipped");
-current++;
-}
+function nextPage() {
+
+  if (current < pages.length) {
+    pages[current].classList.add("flipped");
+    current++;
+
+    // 🎁 END MESSAGE AFTER LAST PAGE
+    if (current === pages.length) {
+      setTimeout(() => {
+        alert("🎁 Wait for the real gifts… something more is coming ✨");
+      }, 800);
+    }
+  }
 }
 
-function prevPage(){
-if(current>0){
-current--;
-pages[current].classList.remove("flipped");
-}
+function prevPage() {
+  if (current > 0) {
+    current--;
+    pages[current].classList.remove("flipped");
+  }
 }
 
-function goHome(){
-window.location.href="index.html";
+function goHome() {
+  window.location.href = "index.html";
 }
